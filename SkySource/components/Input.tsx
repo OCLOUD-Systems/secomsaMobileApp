4db2297b6a6b381f63c0cfe79bf3886cdfb20735
RomuluSremusRom <okadizrafael@gmail.com>
2025-07-22 20:33:23 -0600

    Se agragaron inputs

--- SkySource/components/Input.tsx
+++ SkySource/components/Input.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { StyleSheet, TextInput } from 'react-native';
 
-const Input = () => {
-  return <div>Input</div>;
+const Input = (props: any) => {
+ const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
+  return(
+    <TextInput
+          style={styles.input}
+          onChangeText={props.onChange}
+          value={props.text}
+          placeholder={props.placeholder}
+          keyboardType={props.keyboardType}
+        />
+  );
 };
 
 export default Input;
--- SkySource/components/InputDescription.tsx
+++ SkySource/components/InputDescription.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { StyleSheet, TextInput } from 'react-native';
 
-const InputDescription = () => {
-  return <div>InputDescription</div>;
+const InputDescription = (props: any) => {
+   const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
+
+  return(
+      <TextInput
+            style={styles.input}
+            onChangeText={props.onChange}
+            value={props.text}
+            placeholder={props.placeholder}
+            keyboardType={props.keyboardType}
+            editable={props.editable}
+            multiline
+            numberOfLines={5}
+          />
+    );
 };
 
 export default InputDescription;
