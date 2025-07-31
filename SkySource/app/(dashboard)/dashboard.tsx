da247d1bbbdd8914c52111f50e3f49b92753334e
RomuluSremusRom <okadizrafael@gmail.com>
2025-07-30 20:11:00 -0600

    Se creo pagina dashboard

--- SkySource/app/(dashboard)/dashboard.tsx
+++ SkySource/app/(dashboard)/dashboard.tsx
@@ -1,10 +1,25 @@
+import Slider from "@/components/Slider";
 import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { Button, Linking, SafeAreaView, Text, View } from "react-native";
 
 const Dashboard = () => {
+  const handlePress = () => {
+    Linking.openURL('https://www.secomsagroup.com/');
+  };
+
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-white dark:bg-gray-900">
-      <Text>Dashboard</Text>
+      <View className="">
+        <View className=""></View> 
+        <View className="">
+          <Slider className=""></Slider>
+        </View>
+        <View className="">
+          <Slider className=""></Slider>
+        </View>
+        <Text className="">Visita nuestro sitio web oficial para mas información</Text>
+        <Button title="Ir al sitio" onPress={handlePress}/>
+      </View>
     </SafeAreaView>
   );
 };
